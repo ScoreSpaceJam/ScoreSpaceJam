d5fd65429b99c45c83def528b4c84c4166c62fb6
Parven05 <parvendranofficial05@gmail.com>
2022-04-30 23:43:39 +0800

    added shooting mechanics

---
+++ Assets/Scripts/ProjectTile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectTile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public int damage;
+    public LayerMask whatIsSolid;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemey take damage");
+                hitInfo.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            DestroyProjectile();
+        }
+
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float offset;
+    public GameObject projectile;
+    public Transform shotpoint;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotpoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
